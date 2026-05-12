a5a84d1c6c17649de1a19911c4daba2b72145d9b
judev <76juandevi@gmail.com>
2026-05-12 14:51:50 +0200

    env

---
+++ newspr/config.py
@@ -0,0 +1,8 @@
+"""Configuración de la aplicación."""
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ.get("API_KEY")
+BASE_URL = "https://newsapi.org/v2/everything"
